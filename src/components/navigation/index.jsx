230913a886c30cded1f47f9d576c79d9996e371e
Lorenzo Locker <lo.locker1202@gmail.com>
2024-06-26 22:58:01 -0300

    fix: fixed first page mobile view

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,13 +1,11 @@
 "use client";
 import { BtnList } from "@/app/data";
-import React from "react";
+import React, { useEffect } from "react";
 import NavButton from "./NavButton";
 import useScreenSize from "../hooks/useScreenSize";
 import ResponsiveComponent from "../ResponsiveComponent";
 import { motion } from "framer-motion";
 import { useScrollBlock } from "./useScrollBlock";
-import { useEffect } from 'react';
-
 
 const container = {
   hidden: { opacity: 0 },
@@ -24,56 +22,58 @@ const Navigation = () => {
   const size = useScreenSize();
   const isLarge = size >= 1024;
   const isMedium = size >= 768;
-	const [blockScroll, allowScroll] = useScrollBlock();
+  const [blockScroll, allowScroll] = useScrollBlock();
 
   useEffect(() => {
-			blockScroll();
-	}, [blockScroll]);
+    blockScroll(); // Block scroll on mount
+    return () => {
+      allowScroll(); // Clean up: Allow scroll when component unmounts
+    };
+  }, [blockScroll, allowScroll]);
 
   return (
-    blockScroll(),
+    <>
       <ResponsiveComponent>
         {({ size }) => {
           return size && size >= 480 ? (
-          <div className="w-full fixed h-screen flex items-center justify-center">
-            <motion.div
-              variants={container}
-              initial="hidden"
-              animate="show"
-              className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group"
-            >
-              {BtnList.map((btn, index) => {
-                const angleRad = (index * angleIncrement * Math.PI) / 180;
-                const radius = isLarge
-                  ? "calc(20vw - 1rem)"
-                  : isMedium
-                  ? "calc(30vw - 1rem)"
-                  : "calc(40vw - 1rem)";
-                const x = `calc(${radius}*${Math.cos(angleRad)})`;
-                const y = `calc(${radius}*${Math.sin(angleRad)})`;
+            <div className="w-full fixed h-screen flex items-center justify-center">
+              <motion.div
+                variants={container}
+                initial="hidden"
+                animate="show"
+                className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group"
+              >
+                {BtnList.map((btn, index) => {
+                  const angleRad = (index * angleIncrement * Math.PI) / 180;
+                  const radius = isLarge
+                    ? "calc(20vw - 1rem)"
+                    : isMedium
+                    ? "calc(30vw - 1rem)"
+                    : "calc(40vw - 1rem)";
+                  const x = `calc(${radius}*${Math.cos(angleRad)})`;
+                  const y = `calc(${radius}*${Math.sin(angleRad)})`;
 
-                return <NavButton key={btn.label} x={x} y={y} {...btn} />;
-              })}
-            </motion.div>
-          </div>
+                  return <NavButton key={btn.label} x={x} y={y} {...btn} />;
+                })}
+              </motion.div>
+            </div>
           ) : (
-          <div className="w-full flex items-center justify-center mt-10">
-            <>
-            <motion.div
-              variants={container}
-              initial="hidden"
-              animate="show"
-              className="w-full flex items-center justify-center space-x-4 mt-20"
-            >
-              {BtnList.map((btn) => (
-                <NavButton key={btn.label} x={0} y={0} {...btn} />
-              ))}
-            </motion.div>
-          </>
-        </div>
-        );
+            <div className="w-full flex items-center justify-center mt-10">
+              <motion.div
+                variants={container}
+                initial="hidden"
+                animate="show"
+                className="w-full flex items-center justify-center space-x-4 mt-20"
+              >
+                {BtnList.map((btn) => (
+                  <NavButton key={btn.label} x={0} y={0} {...btn} />
+                ))}
+              </motion.div>
+            </div>
+          );
         }}
       </ResponsiveComponent>
+    </>
   );
 };
 
